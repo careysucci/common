53d0300ee7244ef321056f65befef3ee239dce05
btestzw <btestzzw@163.com>
2024-08-13 23:58:02 +0800

    tst

--- common.sh
+++ common.sh
@@ -336,9 +336,9 @@ fi
 z="*luci-theme-argon*,*luci-app-argon-config*,*luci-theme-Butterfly*,*luci-theme-netgear*,*luci-theme-atmaterial*, \
 luci-theme-rosy,luci-theme-darkmatter,luci-theme-infinityfreedom,luci-theme-design,luci-app-design-config, \
 luci-theme-bootstrap-mod,luci-theme-freifunk-generic,luci-theme-opentomato,luci-theme-kucat, \
-luci-app-eqos,luci-app-wol,luci-app-openclash,kmod-ixgbe,kmod-ixgbevf, \
-luci-app-gost,gost,luci-app-wizard,nano,dnsmasq-full,dropbear \
-luci-app-ssr-plus,*luci-app-passwall2,luci-app-vssr,lua-maxminddb,v2dat,v2ray-geodata,"
+luci-app-eqos,adguardhome,luci-app-adguardhome,mosdns,luci-app-mosdns,luci-app-wol,luci-app-openclash, \
+luci-app-gost,gost,luci-app-smartdns,smartdns,luci-app-wizard,luci-app-msd_lite,msd_lite, \
+luci-app-ssr-plus,*luci-app-passwall*,luci-app-vssr,lua-maxminddb,v2dat,v2ray-geodata"
 t=(${z//,/ })
 for x in ${t[@]}; do \
   find . -type d -name "${x}" |grep -v 'danshui\|small\|freifunk\|danshui1\|passwall3' |xargs -i rm -rf {}; \
